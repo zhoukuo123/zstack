9e7a3ce56f3374bc46a9358d86b7a5b178df4963
zhoukuo123 <1178053078@qq.com>
2021-09-28 14:36:38 +0800

    add rate limit switch test case and improve test case

--- test/src/test/groovy/org/zstack/test/integration/kvm/vm/ratelimit/RateLimitCase.groovy
+++ test/src/test/groovy/org/zstack/test/integration/kvm/vm/ratelimit/RateLimitCase.groovy
@@ -36,9 +36,85 @@ class RateLimitCase extends SubCase {
     @Override
     void test() {
         env.create {
-            int n = 10000
+            int n = 2000
             testRebootVmRateLimit(n)
+            testRateLimitSwitch(n)
         }
+    }
+
+    void testRateLimitSwitch(int n) {
+        VmInstanceInventory vm = env.inventoryByName("vm")
+        def count = new AtomicInteger(0)
+        def passedCount = new AtomicInteger(0)
+        def limitedCount = new AtomicInteger(0)
+
+        StopWatch sw = Utils.getStopWatch()
+        sw.start()
+        for (int i = 0; i < n; i++) {
+            new RebootVmInstanceAction(
+                    uuid: vm.uuid,
+                    sessionId: adminSession(),
+            ).call(new Completion<RebootVmInstanceAction.Result>() {
+                @Override
+                void complete(RebootVmInstanceAction.Result ret) {
+                    count.incrementAndGet()
+                    if (ret.error == null) {
+                        passedCount.incrementAndGet()
+                    } else {
+                        limitedCount.incrementAndGet()
+                    }
+                }
+            })
+        }
+        sw.stop()
+
+        while (count.get() < n) {
+            TimeUnit.SECONDS.sleep(1)
+        }
+
+        def useTime = sw.getLapse() / 1000.0
+        def testPassedCount = calculatePassedCount(useTime)
+        def testApiMsgQPS = calculateQPS(passedCount, useTime)
+        def apiMsgQPS = RateLimitGlobalConfig.API_ASYNC_CALL_MSG_QPS.value(Integer.class)
+
+        assert passedCount.intValue() < testPassedCount + 30 && passedCount.intValue() > testPassedCount - 30
+        assert Math.abs(testApiMsgQPS - apiMsgQPS) < 0.2
+
+        logger.info(String.format("Passed $passedCount APIs", passedCount))
+        logger.info(String.format("Limited to $limitedCount APIs", limitedCount))
+        logger.info(String.format("The test message QPS is $testApiMsgQPS", testApiMsgQPS))
+
+        RateLimitGlobalConfig.RATE_LIMIT_SWITCH.updateValue("false")
+
+        passedCount.set(0)
+        limitedCount.set(0)
+        count.set(0)
+
+        for (int i = 0; i < n; i++) {
+            new RebootVmInstanceAction(
+                    uuid: vm.uuid,
+                    sessionId: adminSession(),
+            ).call(new Completion<RebootVmInstanceAction.Result>() {
+                @Override
+                void complete(RebootVmInstanceAction.Result ret) {
+                    count.incrementAndGet()
+                    if (ret.error == null) {
+                        passedCount.incrementAndGet()
+                    } else {
+                        limitedCount.incrementAndGet()
+                    }
+                }
+            })
+        }
+
+        while (count.get() < n) {
+            TimeUnit.SECONDS.sleep(1)
+        }
+
+        assert passedCount.intValue() == n
+
+        logger.info(String.format("Passed $passedCount APIs", passedCount))
+        logger.info(String.format("Limited to $limitedCount APIs", limitedCount))
     }
 
     void testRebootVmRateLimit(int n) {
@@ -72,15 +148,16 @@ class RateLimitCase extends SubCase {
         }
 
         def useTime = sw.getLapse() / 1000.0
-        def LAST_MSG_QPS = RateLimitGlobalConfig.API_ASYNC_CALL_MSG_QPS.value(Integer.class)
-        def TEST_MSG_QPS = (passedCount.intValue() - LAST_MSG_QPS) / useTime
+        def testPassedCount = calculatePassedCount(useTime)
+        def testApiMsgQPS = calculateQPS(passedCount, useTime)
+        def apiMsgQPS = RateLimitGlobalConfig.API_ASYNC_CALL_MSG_QPS.value(Integer.class)
 
-        assert passedCount.intValue() < n && passedCount.intValue() > 0
-        assert Math.abs(TEST_MSG_QPS - LAST_MSG_QPS) < 0.2
+        assert passedCount.intValue() < testPassedCount + 30 && passedCount.intValue() > testPassedCount - 30
+        assert Math.abs(testApiMsgQPS - apiMsgQPS) < 0.2
 
         logger.info(String.format("Passed $passedCount APIs", passedCount))
         logger.info(String.format("Limited to $limitedCount APIs", limitedCount))
-        logger.info(String.format("The test message QPS is $TEST_MSG_QPS", TEST_MSG_QPS))
+        logger.info(String.format("The test message QPS is $testApiMsgQPS", testApiMsgQPS))
 
         RateLimitGlobalConfig.API_ASYNC_CALL_MSG_QPS.updateValue((RateLimitGlobalConfig.API_ASYNC_CALL_MSG_QPS.value(Integer.class) * 2))
 
@@ -112,14 +189,36 @@ class RateLimitCase extends SubCase {
         }
 
         def newUseTime = sw.getLapse() / 1000.0
-        def NEW_MSG_QPS = RateLimitGlobalConfig.API_ASYNC_CALL_MSG_QPS.value(Integer.class)
-        def NEW_TEST_MSG_QPS = (passedCount.intValue() - NEW_MSG_QPS) / newUseTime
+        def newTestPassedCount = calculatePassedCount(newUseTime)
+        def newTestApiMsgQPS = calculateQPS(passedCount, newUseTime)
+        def newApiMsgQPS = RateLimitGlobalConfig.API_ASYNC_CALL_MSG_QPS.value(Integer.class)
 
-        assert passedCount.intValue() < n && passedCount.intValue() > 0
-        assert Math.abs(NEW_TEST_MSG_QPS - NEW_MSG_QPS) < 0.2
+        assert passedCount.intValue() < newTestPassedCount + 30 && passedCount.intValue() > newTestPassedCount - 30
+        assert Math.abs(newTestApiMsgQPS - newApiMsgQPS) < 0.2
 
         logger.info(String.format("Passed $passedCount APIs", passedCount))
         logger.info(String.format("Limited to $limitedCount APIs", limitedCount))
-        logger.info(String.format("The test message QPS is $TEST_MSG_QPS", TEST_MSG_QPS))
+        logger.info(String.format("The test message QPS is $newTestApiMsgQPS", newTestApiMsgQPS))
+    }
+
+    Object calculatePassedCount(Object useTime) {
+        def apiMsgQPS = RateLimitGlobalConfig.API_ASYNC_CALL_MSG_QPS.value(Integer.class)
+        def apiMsgCapacity = apiMsgQPS
+        def testPassedCount = (apiMsgQPS * useTime < apiMsgCapacity)
+                ? apiMsgQPS * useTime
+                : apiMsgQPS * useTime + apiMsgCapacity
+        return testPassedCount
+    }
+
+    Object calculateQPS(AtomicInteger passedCount, Object useTime) {
+        def apiMsgQPS = RateLimitGlobalConfig.API_ASYNC_CALL_MSG_QPS.value(Integer.class)
+        def apiMsgCapacity = apiMsgQPS
+        def testMsgQPS
+        if (passedCount.intValue() > apiMsgCapacity) {
+            testMsgQPS = (passedCount.intValue() - apiMsgCapacity) / useTime
+        } else {
+            testMsgQPS = passedCount.intValue() / useTime
+        }
+        return testMsgQPS
     }
 }
